59730697358e1bfd2a027b17ed9760589b444ce3
juan jose marin valencia <gestordesistemasbitwan@gmail.com>
2023-06-13 15:15:22 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cuadrillas',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
